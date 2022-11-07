26cca6ec64bc590e56c5a1e3ff667df117c5a967
sheldon <997166273@qq.com>
2022-11-07 17:11:52 +0800

    cherry-pick: sm2

--- testutil/testdata/tx.go
+++ testutil/testdata/tx.go
@@ -36,7 +36,7 @@ func NewTestFeeAmount() sdk.Coins {
 
 // NewTestGasLimit is a test fee gas limit.
 func NewTestGasLimit() uint64 {
-	return 200000
+	return 400000
 }
 
 // NewTestMsg creates a message for testing with the given signers.
